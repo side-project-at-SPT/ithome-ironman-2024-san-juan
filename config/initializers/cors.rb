63bdb1694eeadced779cf2dc5fbd663ecab42232
noracami <kerke.2011@gmail.com>
2024-09-07 02:38:05 +0800

    💚 fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "*"
 
-    resource [ "/api/v1/*", "walking-skeleton" ],
+    resource "walking-skeleton", headers: :any, methods: :any
+    resource "/api/v1/*",
       headers: :any,
       methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
   end
