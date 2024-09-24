0cc84f9a2af72c309be0b7dde2e00968d5f0b359
noracami <kerke.2011@gmail.com>
2024-09-24 16:30:41 +0800

    🚑 Add localhost:5173 to allowed request origins in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,6 +33,7 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [ "http://localhost:5173" ]
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
